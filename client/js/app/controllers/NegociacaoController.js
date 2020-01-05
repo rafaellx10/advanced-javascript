82deddf99b97b177f7884f0991d31a28ebc0a0cf
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-01-05 14:43:44 -0300

    Connect NegociacaoController with index.html

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,14 @@
+class NegociacaoController {
+	adiciona(event) {
+		event.preventDefault();
+
+		let $ = document.querySelector.bind(document);
+		let inputData = $("#data");
+		let inputQuantidade = $("#quantidade");
+		let inputvalor = $("#valor");
+
+		console.log(inputData.value);
+		console.log(inputQuantidade.value);
+		console.log(inputvalor.value);
+	}
+}
