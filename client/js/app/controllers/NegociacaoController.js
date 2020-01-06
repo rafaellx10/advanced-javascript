37408dd6126bb3ecd7ed7ee782e3c8edbf4b3d39
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-01-05 22:21:04 -0300

    replace standard function() to arrow function

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -8,20 +8,17 @@ class NegociacaoController {
 	adiciona(event) {
 		event.preventDefault();
 
-		console.log(new Date("2020,11,05"));
-		console.log(new Date("2020", "10", "06"));
-		// console.log(new Date(2020, 11, 06));
-		//["2020", "10", "06"].join(',');
-		let data = new Date(this._inputData.value.split("-"));
-		let data2 = new Date(this._inputData.value.replace(/-/g, ","));
-		let data3 = new Date(
-			...this._inputData.value.split("-").map(function(item, indice) {
-				return item - (indice % 2);
-			})
+		let data = new Date(
+			...this._inputData.value
+				.split("-")
+				.map((item, indice) => item - (indice % 2))
 		);
-		console.log(typeof data);
-		console.log(data);
-		console.log(data2);
-		console.log(data3);
+
+		let negociacao = new Negociacao(
+			data,
+			this._inputQuantidade.value,
+			this._inputvalor.value
+		);
+		console.log(negociacao);
 	}
 }
