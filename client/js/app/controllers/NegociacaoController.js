8c960ada2c6c26981cf01e7960f8543f42e88b97
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-01-05 21:36:15 -0300

    test: ways to manipulate dates

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -8,8 +8,20 @@ class NegociacaoController {
 	adiciona(event) {
 		event.preventDefault();
 
-		console.log(typeof this._inputData.value);
-
-		console.log(this._inputData.value);
+		console.log(new Date("2020,11,05"));
+		console.log(new Date("2020", "10", "06"));
+		// console.log(new Date(2020, 11, 06));
+		//["2020", "10", "06"].join(',');
+		let data = new Date(this._inputData.value.split("-"));
+		let data2 = new Date(this._inputData.value.replace(/-/g, ","));
+		let data3 = new Date(
+			...this._inputData.value.split("-").map(function(item, indice) {
+				return item - (indice % 2);
+			})
+		);
+		console.log(typeof data);
+		console.log(data);
+		console.log(data2);
+		console.log(data3);
 	}
 }
