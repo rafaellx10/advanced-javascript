bc7e6027cd0060e188d1e08aa5f52731d299047b
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-01-05 23:26:21 -0300

    refactor(NegociacaoController):  organize class with more functions

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -4,17 +4,29 @@ class NegociacaoController {
 		this._inputData = $("#data");
 		this._inputQuantidade = $("#quantidade");
 		this._inputvalor = $("#valor");
+		this._listaNegociacoes = new ListaNegociacoes();
 	}
 	adiciona(event) {
 		event.preventDefault();
 
-		let negociacao = new Negociacao(
+		this._listaNegociacoes.adiciona(this._criaNegociacao());
+		this._limpaForm();
+		console.log(this._listaNegociacoes.negociacoes);
+	}
+
+	_criaNegociacao() {
+		return new Negociacao(
 			DateHelper.textoParaData(this._inputData.value),
 			this._inputQuantidade.value,
 			this._inputvalor.value
 		);
-		console.log(negociacao);
+	}
 
-		console.log(DateHelper.dataParaTexto(negociacao.data));
+	_limpaForm() {
+		this._inputData.value = "";
+		this._inputQuantidade.value = 1;
+		this._inputvalor.value = 0.0;
+
+		this._inputData.focus();
 	}
 }
