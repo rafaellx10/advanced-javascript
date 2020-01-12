61543a6317d7e8cb1161912fd9f62c82ca6ae2df
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-01-12 00:58:37 -0300

    feat: add importarNegociacoes()

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,24 +6,21 @@ class NegociacaoController {
 		this._inputvalor = $("#valor");
 
 		let self = this;
-		this._listaNegociacoes = new Proxy(
-			new ListaNegociacoes(new Date(), 1, 100),
-			{
-				get(target, prop, receiver) {
-					if (
-						["adiciona", "esvazia"].includes(prop) &&
-						typeof target[prop] == typeof Function
-					) {
-						return function() {
-							console.log(`interceptando ${prop}`);
-							Reflect.apply(target[prop], target, arguments);
-							self._negociacoesView.update(target);
-						};
-					}
-					return Reflect.get(target, prop, receiver);
+		this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+			get(target, prop, receiver) {
+				if (
+					["adiciona", "esvazia"].includes(prop) &&
+					typeof target[prop] == typeof Function
+				) {
+					return function() {
+						console.log(`interceptando ${prop}`);
+						Reflect.apply(target[prop], target, arguments);
+						self._negociacoesView.update(target);
+					};
 				}
+				return Reflect.get(target, prop, receiver);
 			}
-		);
+		});
 		// this._listaNegociacoes = new ListaNegociacoes(model => {
 		// 	console.log(this);
 		// 	this._negociacoesView.update(model);
@@ -44,6 +41,52 @@ class NegociacaoController {
 		this._mensagemViem.update(this._mensagem);
 
 		this._limpaForm();
+	}
+
+	importarNegociacoes() {
+		console.log("Importando negociacoes");
+		let xhr = new XMLHttpRequest();
+
+		xhr.open("GET", "negociacoes/semana");
+
+		/*configuracoes*/
+		/*  Estados
+			0: requisição ainda não iniciada
+            1: conexão com o servidor estabelecida
+            2: requisição recebida
+            3: processando requisição
+            4: requisição está concluída e a resposta está pronta
+        */
+		xhr.onreadystatechange = () => {
+			if (xhr.readyState == 4) {
+				if (xhr.status == 200) {
+					console.log("obtendo as negociações do servidor");
+					console.log(JSON.parse(xhr.responseText));
+					JSON.parse(xhr.responseText).map(objeto => {
+						this._listaNegociacoes.adiciona(
+							new Negociacao(
+								new Date(objeto.data),
+								objeto.quantidade,
+								objeto.valor
+							)
+						);
+					});
+					this._mensagem.texto = "Negociações importadas com sucesso";
+					this._mensagemViem.update(this._mensagem);
+				} else {
+					console.log(xhr.responseText);
+					this._mensagem.texto =
+						"Não foi possível obter as negociações da semana";
+					this._mensagemViem.update(this._mensagem);
+					console.log(
+						"Não foi possível obter as negociações do servidor"
+					);
+				}
+			}
+		};
+
+		/* Executar */
+		xhr.send();
 	}
 
 	apaga() {
