44676047b9abd0c775c9136950b3c1e54092ebbe
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-01-12 23:28:27 -0300

    (ConnectionFactory.js) use Module Pattern

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,44 +1,48 @@
-var stores = ["negociacoer"];
-var version = 5;
-var dbName = "aluraframe";
+function tmp() {
+	var stores = ["negociacoer"];
+	var version = 5;
+	var dbName = "aluraframe";
 
-var connection = null;
+	var connection = null;
 
-class ConnectionFactory {
-	constructor() {
-		throw new Error(
-			"It is not possible to create new instace of ConnectionFactory"
-		);
-	}
+	return class ConnectionFactory {
+		constructor() {
+			throw new Error(
+				"It is not possible to create new instace of ConnectionFactory"
+			);
+		}
 
-	static getConnection() {
-		return new Promise((resolve, reject) => {
-			let openRequest = window.indexedDB.open(dbName, version);
+		static getConnection() {
+			return new Promise((resolve, reject) => {
+				let openRequest = window.indexedDB.open(dbName, version);
 
-			openRequest.onupgradeneeded = e => {
-				ConnectionFactory._createStores(e.target.result);
-			};
+				openRequest.onupgradeneeded = e => {
+					ConnectionFactory._createStores(e.target.result);
+				};
 
-			openRequest.onsuccess = e => {
-				if (!connection) {
-					connection = e.target.result;
-				}
-				resolve(connection);
-			};
+				openRequest.onsuccess = e => {
+					if (!connection) {
+						connection = e.target.result;
+					}
+					resolve(connection);
+				};
 
-			openRequest.onerror = e => {
-				console.log(e.target.Error);
-				reject(e.target.erro.name);
-			};
-		});
-	}
+				openRequest.onerror = e => {
+					console.log(e.target.Error);
+					reject(e.target.erro.name);
+				};
+			});
+		}
 
-	static _createStores(connection) {
-		stores.forEach(store => {
-			if (connection.objectStoreNames.contains(stores))
-				connection.deleteObjectStore(store);
+		static _createStores(connection) {
+			stores.forEach(store => {
+				if (connection.objectStoreNames.contains(stores))
+					connection.deleteObjectStore(store);
 
-			connection.createObject(store, { autoIncrement: true });
-		});
-	}
+				connection.createObject(store, { autoIncrement: true });
+			});
+		}
+	};
 }
+
+var ConnectionFactory = tmp();
